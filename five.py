37121aeb26c39abdfe99c243d819ae0e0c5bd0bb
saikrishna1103 <45629193+saikrishna1103@users.noreply.github.com>
2019-07-03 10:03:36 +0530

    Update five.py

--- five.py
+++ five.py
@@ -1,10 +1,9 @@
-s=int(input())
-a=int(input())
-i=int(input())
+s,a,i=map(int,input().split())
 if((s>=a) and (s>=i)) :
 	print(s)
 elif((a>=i) and (a>=s)) :
 	print(a)
 elif((i>=a) and (i>=s)) :
 	print(i)
+
 
