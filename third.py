e1d891f3d8c991bd612ca8cd44742d57c2ed47ae
saikrishna1103 <45629193+saikrishna1103@users.noreply.github.com>
2019-07-03 09:44:05 +0530

    Create third.py

---
+++ third.py
@@ -0,0 +1,9 @@
+s=str(input())
+sai=["a","e","i","o","u"]
+krish=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if s in sai:
+    print("Vowel")
+elif s in krish:
+    print("Consonant")
+else:
+    print("Invalid")
