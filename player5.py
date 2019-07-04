9e5182901ef39733e631244ad7fe5c70db1c3549
saikrishna1103 <45629193+saikrishna1103@users.noreply.github.com>
2019-07-04 12:19:21 +0530

    Create player5.py

---
+++ player5.py
@@ -0,0 +1,3 @@
+s=str(input())
+roman_numerals = ["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XV","XVI","XVII","XVIII","XIX","XX"]
+print(roman_numerals.index(s)+1)
