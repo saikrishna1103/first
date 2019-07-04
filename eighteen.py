3239221e6e900e9c884de747794892fb43127003
saikrishna1103 <45629193+saikrishna1103@users.noreply.github.com>
2019-07-04 11:56:02 +0530

    Update eighteen.py

--- eighteen.py
+++ eighteen.py
@@ -1,15 +1,10 @@
-lower,upper=map(int,input().split())
-for num in range(lower, upper + 1):
-
-   order = len(str(num))
-
-   sum = 0
-
-   temp = num
-   while temp > 0:
-       digit = temp % 10
-       sum += digit ** order
-       temp //= 10
-
-   if num == sum:
-       print(num,end=' ')
+b1,b2=map(int,input().split())
+for n in range(b1+1,b2):
+  sum=0
+  temp1=n
+  while temp1>0:
+    digit=temp1%10
+    sum+=digit**3
+    temp1//=10
+  if n==sum:
+    print(n,end=' ')
